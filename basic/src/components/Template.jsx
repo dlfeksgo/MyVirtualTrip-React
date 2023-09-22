e3ea495fef3ff5baa2600612cbf310ce658c1dfb
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2023-09-22 15:29:05 +0900

    사용하지 않는 컴포넌트 제거

--- basic/src/components/Template.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Template.module.css";
-
-const Template = ({ children }) => {
-    return <div className={styles.container}>{children}</div>;
-};
-
-export default Template;
