3de8566a29331c0eb5fc578dba81bfacdce64947
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2023-11-07 16:36:43 +0900

    orderId로 개별 렌더링

--- 04-recoil/src/components/Order/OrderItem.jsx
+++ 04-recoil/src/components/Order/OrderItem.jsx
@@ -1,20 +1,20 @@
 import React, { memo } from "react";
-import { orderItem, useOrderAction } from "../../atoms/order";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { orderItem } from "../../atoms/order";
+import { useRecoilState } from "recoil";
 
 const OrderItem = ({ orderId }) => {
   console.log("OrderItem Render");
-  const { update } = useOrderAction();
-  const [[order], setOrder] = useRecoilState(orderItem(orderId));
+  // const { update } = useOrderAction(orderId);
+  const [order, setOrder] = useRecoilState(orderItem(orderId));
 
   const handleItemClick = () => {
-    // dispatch(orderSlice.actions.update(order.id));
     // update(order.id);
-    setOrder((prev) =>
-      prev.map((v) =>
-        v.id === order.id ? { ...v, isCompleted: !v.isCompleted } : v
-      )
-    );
+    // setOrder((prev) =>
+    //   prev.map((v) =>
+    //     v.id === order.id ? { ...v, isCompleted: !v.isCompleted } : v
+    //   )
+    // );
+    setOrder([{ ...order, isCompleted: !order.isCompleted }]);
   };
 
   return (
